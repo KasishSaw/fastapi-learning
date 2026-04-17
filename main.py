7eb11998d10b11bf0a26bb502ee4d91709c435b0
Kasish <kasishsaw098@gmail.com>
2026-04-17 07:00:06 +0530

    feat:path and query parameters

--- main.py
+++ main.py
@@ -5,3 +5,35 @@ app=FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id}
+
+@app.get("/items")
+def get_items(skip: int = 0, limit: int = 10):
+    return {"skip": skip, "limit": limit}
+
+from typing import Optional
+
+@app.get("/products")
+def get_products(category: Optional[str] = None):
+    if category:
+        return {"category": category}
+    return {"message": "showing all products"}
+
+@app.get("/users/{user_id}/orders")
+def get_user_orders(user_id: int, limit: int = 5, status: Optional[str] = None):
+    return {
+        "user_id": user_id,
+        "limit": limit,
+        "status": status
+    }
+
+@app.get("/products/{product_id}")
+def get_product_id(product_id:int, discount: float=0.0):
+    return{"product_id":product_id, "discount": discount}
+
+@app.get("/orders")
+def orders(status:Optional[str]=None, limit: int = 10):
+    return {"status":status, "limit":limit}
